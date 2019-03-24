701699ccdd41434ecb17ff68cd62b42c2245bb46
Aditya Respati <dityo.hario@gmail.com>
2019-03-24 15:51:10 +0700

    recommendations

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const routes = require('express').Router()
 const userController = require('../controllers/userController')
 const UserRoutes = require('../routes/user')
 const MeetupsRoutes = require('./meetups')
+const PlacesController = require('../controllers/PlacesController')
 const images = require('../helpers/images.js')
 
 routes.post('/register', images.multer.single('image'), images.sendUploadToGCS , userController.register)
 routes.post('/login', userController.login)
+routes.post('/recommendations', PlacesController.getReccomendedByCity)
 
 routes.use('/meetups', MeetupsRoutes)
 routes.use('/users', UserRoutes)
